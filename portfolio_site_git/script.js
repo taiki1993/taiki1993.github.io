3bf1472d066e95fdf1cb66720cbf82a1f956413c
taiki yamane <taikiiiiii1214@yahoo.co.jp>
2018-01-11 17:39:38 +0900

    up

--- portfolio_site_git/script.js
+++ portfolio_site_git/script.js
@@ -8,3 +8,20 @@ $(function(){
         $('header nav').addClass('mobile-menu-open').remove('fade-in')
     });
 });
+
+$(function(){
+    var stage = new createjs.Stage('stage');
+
+    var circle = new createjs.Shape();
+            circle.graphics.beginFill('#DF01D7').drawCircle(0,0,30);
+            stage.addChild(circle); //ステージの配置
+            circle.x = 50;
+            circle.y = 50;
+
+            function nextAnime(){
+                console.log('next animation');
+                circle.scaleX = 1;
+                circle.scaleY = 1;
+            }
+
+});
